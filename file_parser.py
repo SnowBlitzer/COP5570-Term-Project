fc2501c8822d617182ac40b3b1b7a20f3d08a353
andrewsosa001 <andrewsosa001@gmail.com>
2016-11-15 11:30:44 -0500

    Adds simple multithreading.
    Add simple multithreading via concurrents.futures package.
    Further improves code readability.

--- file_parser.py
+++ file_parser.py
@@ -1,5 +1,8 @@
-import os, sys
+import os, sys, concurrent.futures
 
+# Global data structures
+emails = dict()
+total_content = list()
 
 def clean_line(lineHandedIn):
 	"""
@@ -15,49 +18,76 @@ def clean_line(lineHandedIn):
 			uniqueSet.add(charStrippedText)
 	return uniqueSet
 
+def analyze_file(file_path):
+	"""
+	Handles a single file path.
+	"""
+
+	addFlag = False
+	content = set()
+	global total_content
+	global emails
+
+	with open(file_path,"r") as file_contents:
+		#Iterate through file contents
+		for line in file_contents:
+			line_tokens = line.split()
+
+			if len(line_tokens) > 1:
+				#if the flag is set, clean up all the data.
+				#Discard links and strip characters. Add it to the list
+				if addFlag:
+					content = content.union(clean_line(line_tokens))
+				#Add the email to the hash table (counter)
+				elif line_tokens[0] == "From:":
+					if line_tokens[-1] in emails:
+						emails[line_tokens[-1]] += 1
+					else:
+						emails[line_tokens[-1]] = 1
+				#If the content is plain/text, set the addFlag
+				elif line_tokens[1] == "text/plain" or line_tokens[1] == "text/plain;":
+					addFlag = True
+			elif len(line_tokens) == 1 and addFlag and not line_tokens[0].isalnum():
+				addFlag = False
+				break
+		total_content.append(content)
 
 def parse_files(path):
 	"""
 	Extracts content from all files within the given
 	path.
 	"""
+
 	# Add missing / if needed
 	if not path.endswith("/"):
 		path += "/"
 
-	emails = dict()
-	content = set()
-	totalContent = list()
-	addFlag = False
+	def qualify(file_path):
+		"""
+		Approves files to be included in analysis.
+		"""
+		return os.path.isfile(file_path) and (file_path.endswith(".lorien"))
 
-	#Look through all files in current directory
-	for filename in os.listdir(path):
-		file_path = path + filename
-		if os.path.isfile(file_path):
-			#Open the file
-			with open(file_path,"r") as file_contents:
-				#Iterate through file contents
-				for line in file_contents:
-					#print line
-					line_tokens = line.split()
-					if len(line_tokens) > 1:
-						#if the flag is set, clean up all the data.
-						#Discard links and strip characters. Add it to the list
-						if addFlag:
-							content = content.union(clean_line(line_tokens))
-						#Add the email to the hash table
-						elif line_tokens[0] == "From:":
-							if line_tokens[-1] in emails:
-								emails[line_tokens[-1]] += 1
-							else:
-								emails[line_tokens[-1]] = 1
-						#If the content is plain/text, set the addFlag
-						elif line_tokens[1] == "text/plain" or line_tokens[1] == "text/plain;":
-							addFlag = True
-					elif len(line_tokens) == 1 and addFlag and not line_tokens[0].isalnum():
-						break
-				totalContent.append(content)
-	print totalContent
+	#counter = 0
+
+	#Look through all qualifying files in path
+	files = (filename for filename in os.listdir(path) if qualify(path + filename))
+	# for filename in files:
+	# 	file_path = path + filename
+	# 	analyze_file(file_path)
+	# 	#counter += 1
+
+	# code from stack_overflow
+	executor = concurrent.futures.ThreadPoolExecutor(10)
+	futures = [executor.submit(analyze_file, path + filename) for filename in files]
+	concurrent.futures.wait(futures)
+
+	final_content = [item for item in total_content if len(item) is not 0]
+
+
+	print final_content
+	#print counter
+	#print emails
 
 if __name__ == '__main__':
 	if len(sys.argv) is not 2:
