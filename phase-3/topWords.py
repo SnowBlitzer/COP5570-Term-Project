399b157819439a3d24dd9608ec8136d385a3c764
Michael Borsellino <michaelborsellino@gmail.com>
2016-12-04 15:57:18 +0000

    Changed topWords to export data

--- phase-3/topWords.py
+++ phase-3/topWords.py
@@ -18,13 +18,20 @@ for message in spams.find():
 
 word_label_list = list()
 word_counter_list = list()
+out_file = open("exportData",'w')
 for word in wordCounter.most_common(100):
-	word_label_list.append(word[0])
-	word_counter_list.append(word[1])
+	out_file.write(word[0])
+	out_file.write(",")
+	out_file.write(str(word[1]))
+	out_file.write(",")
+	#word_label_list.append(word[0])
+	#word_counter_list.append(word[1])
 
+"""
 spread = np.arange(100)
 plt.bar(spread, word_counter_list, 0.35, color='r')
 plt.set_xticks(spread)
 plt.set_xticklabels(word_label_list)
 
 plt.show()
+"""
