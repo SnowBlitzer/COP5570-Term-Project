3c8581a076cc8536c2edce2c31280785158ea693
Michael Borsellino <michaelborsellino@gmail.com>
2016-12-04 11:44:18 -0500

    Made plotter more generic

--- phase-3/plotFromExport.py
+++ phase-3/plotFromExport.py
@@ -1,16 +1,17 @@
 from collections import Counter
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
 
 word_counter_list = list()
 word_label_list = list()
-with open("exportData","r") as file:
+with open(sys.argv[1],"r") as file:
 	for line in file:
 		word_counter_list =  line.split(",")[1::2]
 		word_label_list = line.split(",")[::2]
 
-spread = np.arange(100)
-plt.bar(spread, word_counter_list, 0.35, color='r')
+spread = np.arange(len(word_counter_list))
+plt.bar(spread, word_counter_list, 0.35, color='r',align='center')
 ax = plt.gca()
 ax.set_xticks(spread)
 ax.set_xticklabels(word_label_list, rotation=90)
